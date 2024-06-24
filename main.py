db4d49f95c15342b07b791af48b45803f3ccee09
jennifer <jennifersantossouza@hotmail.com>
2024-06-24 13:57:10 -0300

    makes the snake move

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(width=500, height=500)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
 
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+
+segments = []
 
 for position in starting_positions:
     new_segment = Turtle(shape="square")
     new_segment.color("white")
+    new_segment.penup()
     new_segment.goto(position)
+    segments.append(new_segment)
 
 
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    for seg in segments:
+        seg.forward(20)
 
 
 
