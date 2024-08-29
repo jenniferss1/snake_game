90c9a51b9b7f1407e9c2c068d5b856788ad6a5fb
jennifer <jennifersantossouza@hotmail.com>
2024-08-29 13:34:59 -0300

    feat: Implementing the scoreboard and food functions

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from turtle import Screen, Turtle
-import time
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
+import time
 
 screen = Screen()
 screen.setup(width=500, height=500)
@@ -9,6 +11,8 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = ScoreBoard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -20,8 +24,12 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    
     snake.move()
+    
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
 
 
 
