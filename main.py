30fa6225fa1e9a3151cb989f1485ab44c5974f8f
jennifer <jennifersantossouza@hotmail.com>
2024-08-31 12:54:52 -0300

    feat: Implementing game over function and snake collision with wall and tail

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from scoreboard import ScoreBoard
 import time
 
 screen = Screen()
-screen.setup(width=500, height=500)
+screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
@@ -29,8 +29,18 @@ while game_is_on:
     # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
+    
+    # Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False 
+        scoreboard.game_over()
 
-
+    # Detect collision with tail 
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -10,12 +10,16 @@ class ScoreBoard(Turtle):
         self.score = 0
         self.color("white")
         self.penup()
-        self.goto(0, 220)
+        self.goto(0, 270)
         self.update_scoreboard()
         self.hideturtle()
     
     def update_scoreboard(self):
         self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+    
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER!", align=ALIGNMENT, font=FONT)
     
     def increase_score(self):
         self.score += 1
